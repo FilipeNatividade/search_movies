439968592c7058ef18591d36ee3312d4c340bc9f
Filipe_Natividade <n_filipe_n@hotmail.com>
2021-09-29 15:43:23 -0300

    add placehold input/done

--- src/components/container-search/index.js
+++ src/components/container-search/index.js
@@ -1,21 +1,20 @@
 import { useStateProvider } from "../../providers/state-context";
-import { Container,InputSearch } from "./style";
+import { Container, InputSearch } from "./style";
 const ContainerSearch = () => {
-  const {title, setTitle} = useStateProvider()
+  const { title, setTitle } = useStateProvider();
 
   const getTitle = (e) => {
-    e.preventDefault()
-    setTitle(e.target.value)
-  }
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
   return (
-  <Container>
+    <Container>
       <h2>Qual filme você procura?</h2>
-      <InputSearch 
-        value={title}
-        onChange={e => getTitle(e)}
-      />
-  </Container>
-  )
+      <div>
+        <InputSearch value={title} onChange={(e) => getTitle(e)} placeholder='Informe o título no idioma de origem'/>
+      </div>
+    </Container>
+  );
 };
 
 export default ContainerSearch;
